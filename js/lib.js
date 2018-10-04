d56d6a47fd943ff21b1599a5aedb386262edd85b
ads226 <ads1024@gmail.com>
2018-10-05 00:44:41 +0900

    Except
    제외 수 필터 만드는 중

--- js/lib.js
+++ js/lib.js
@@ -2,3 +2,67 @@ Array.prototype.random = function() {
 	return this[Math.floor(this.length * Math.random())];
 }
 
+Array.prototype.max = function() {
+	return this.reduce(function(prev, cur) {
+		return Math.max(prev, cur);
+	});
+}
+
+Array.prototype.min = function() {
+	return this.reduce(function(prev, cur) {
+		return Math.min(prev, cur);
+	});
+}
+
+
+function UI_Extention(element, wrap) {
+//	var isOpened = 1,	// 0: false, 1: true
+	var isOpened = (element.innerHTML === "+") ? 0 : 1,
+		wrap = wrap || element.parentElement.parentElement;
+	
+	element.onclick = function(e) {
+		isOpened = (isOpened + 1) % 2;
+		element.innerHTML = (isOpened) ? "-" : "+";
+		
+		this.open(isOpened, wrap);
+		
+	}.bind(this);
+	
+	this.open(isOpened, wrap);
+	
+	wrap.style.overflow = "hidden";
+	wrap.style.transitionDuration = "0.2s";
+}
+
+UI_Extention.prototype.open = function(value, target) {
+	target.style.height = (value) ? target.scrollHeight + "px" : "50px";
+}
+
+
+
+function createLottoDataAll() {
+	var i = 0,
+		res = new Array(8145060);
+	
+	for (var a = 1; a <= 40; ++ a) {
+
+		for (var b = a + 1; b <= 41; ++ b) {
+
+			for (var c = b + 1; c <= 42; ++ c) {
+
+				for (var d = c + 1; d <= 43; ++ d) {
+
+					for (var e = d + 1; e <= 44; ++ e) {
+
+						for (var f = e + 1; f <= 45; ++ f) {
+							res[i] = [a, b, c, d, e, f];
+							i++;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	return res;
+}
--- js/lotto.js
+++ js/lotto.js
@@ -1,9 +1,44 @@
+var exceptNum = 1;
+
 window.onload = function() {
 	console.time("[Time to] ++++++++++ Page load completed");
 	
-	
-	
-	
+	initFilterExtension();
+	initExceptNum();
 	
 	console.timeEnd("[Time to] ++++++++++ Page load completed");
+}
+
+function initFilterExtension() {
+	var uis = document.querySelectorAll(".filter_extension"),
+		wraps = document.querySelectorAll(".filter")
+		i = 0,
+		len = uis.length;
+	
+	for (; i < len; ++ i) {
+		new UI_Extention(uis[i], wraps[i]);
+	}
+}
+
+function initExceptNum() {
+	var tds = document.querySelectorAll("#except_condition_num td"),
+		i = 0,
+		len = tds.length;
+	
+	for (; i < len; ++ i) {
+		var td = tds[i];
+		td.idx = i + 1;
+		
+		if (td.idx === exceptNum) td.classList.add("selected");
+		else td.classList.remove("selected");
+		
+		td.onclick = function(e) {
+			if (e.target.idx === exceptNum) return;
+			
+			tds[exceptNum - 1].classList.remove("selected");
+			tds[e.target.idx - 1].classList.add("selected");
+			
+			exceptNum = e.target.idx;
+		}
+	}
 }
